740c0ecdadded891f86c55107c2ed349925c57c2
playbench-dev <dev@playbench.co.kr>
2023-10-13 13:27:05 +0900

    merge test

--- pages/admin/user.vue
+++ pages/admin/user.vue
@@ -2,18 +2,10 @@
   <div>
     Index
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <div>TTTTTTT</div>
-=======
   <div>Index
-=======
-  <div>안녕 민우우우
->>>>>>> e6c67f7c5130f7ad08c7a0f0fc192a856e548ff1
 
  <div>Index</div>
  <div>Index</div> <div>Index</div> <div>Index</div> <div>Index</div> <div>Index</div> <div>Index</div> <div>Index</div>
->>>>>>> 30430db940d651fd1246e7e13c8896fbcc5e53d6
   </div>
 </template>
 <script>
